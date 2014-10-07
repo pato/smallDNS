e2523a7df98b40f9b4e2861db458a9e2e67bcb82
Pato <plankenau@gmail.com>
2014-10-06 19:34:30 -0500

    added simple DNS server, takes dns queries and updates the database

---
+++ dnsServer.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+from __future__ import print_function
+from json import dumps, loads
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+
+PORT_NUMBER = 7979
+DNS = dict()
+
+"""
+Used for handling requests
+
+Valid Requests:
+  /hosts - print the entries in DNS
+  /ipaddr|name - update the entry for name with new ipaddr
+"""
+class RequestHandler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    if self.path == "/hosts":
+      self.send_response(200)
+      self.send_header('Content-type','text/html')
+      self.end_headers()
+      self.wfile.write(strDNS())
+      return
+    if "/" not in self.path or "~" not in self.path:
+      self.send_response(400)
+      self.send_header("Content-type","text/html")
+      self.end_headers()
+      self.wfile.write("Bad Request")
+    else:
+      _, getvars = self.path.split("/")
+      ipaddr, hostname = getvars.split("~")
+      self.send_response(200)
+      self.send_header('Content-type','text/html')
+      self.end_headers()
+      self.wfile.write("Updating " + hostname + " to " + ipaddr)
+      DNS[hostname] = ipaddr
+      writeDNS()
+    return
+
+"""
+Creates a string representation of the DNS entries
+Useful because you can append it to /etc/hosts
+"""
+def strDNS():
+  ret = ""
+  for hostname, ipaddr in DNS.iteritems():
+    ret += ipaddr + "\t" + hostname + "\n"
+  return ret
+
+"""
+Serialized the current DNS entries as json
+"""
+def writeDNS():
+  localDNS = open(".localDNS", "w+")
+  print(dumps(DNS), file=localDNS)
+
+"""
+Deserialize the DNS entries saved on disk
+"""
+def loadDNS():
+  localDNS = open(".localDNS", "r")
+  global DNS
+  DNS = loads(localDNS.read())
+
+if __name__ == "__main__":
+  try:
+    server = HTTPServer(('', PORT_NUMBER), RequestHandler)
+    loadDNS()
+    print("Loaded previous DNS configuration")
+    print(DNS)
+    print("Started DNS server on port" , PORT_NUMBER)
+    server.serve_forever()
+  except KeyboardInterrupt:
+    print("^C received, shutting down the web server")
+    server.socket.close()
